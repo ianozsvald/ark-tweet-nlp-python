65512b215654f4dec58e9de3f445f0ed5f9db1f0
Ian Ozsvald <ian@ianozsvald.com>
2013-03-27 16:27:24 +0000

    first

---
+++ CMUTweetTagger.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Simple Python wrapper for runTagger.sh script for CMU's Tweet Tokeniser and Part of Speech tagger: http://www.ark.cs.cmu.edu/TweetNLP/
+
+Usage:
+results=runtagger_parse(['example tweet 1', 'example tweet 2'])
+results will contain a list of lists (one per tweet) of triples, each triple represents (term, type, confidence)
+"""
+import subprocess
+import os
+
+# The only relavent source I've found is here:
+# http://m1ked.com/post/12304626776/pos-tagger-for-twitter-successfully-implemented-in
+# which is a very simple implementation, my implementation is a bit more
+# useful (but not much).
+
+RUN_TAGGER_CMD = "./runTagger.sh"
+
+
+def _split_results(rows):
+    """Parse the tab-delimited returned lines, modified from: https://github.com/brendano/ark-tweet-nlp/blob/master/scripts/show.py"""
+    for line in rows:
+        line = line.strip()  # remove '\n'
+        if len(line) > 0:
+            if line.count('\t') == 2:
+                parts = line.split('\t')
+                tokens = parts[0]
+                tags = parts[1]
+                confidence = float(parts[2])
+                yield tokens, tags, confidence
+
+
+def _call_runtagger(tweets, run_tagger_cmd=RUN_TAGGER_CMD):
+    """Call runTagger.sh using a named input file"""
+    if not os.path.exists(run_tagger_cmd):
+        raise ValueError("Cannot find \"%s\"" % (run_tagger_cmd))
+    message = "\n".join(tweets)
+    po = subprocess.Popen([run_tagger_cmd, '--output-format', 'conll'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = po.communicate(message)
+    # expect a tuple of 2 items like:
+    # ('hello\t!\t0.9858\nthere\tR\t0.4168\n\n',
+    # 'Listening on stdin for input.  (-h for help)\nDetected text input format\nTokenized and tagged 1 tweets (2 tokens) in 7.5 seconds: 0.1 tweets/sec, 0.3 tokens/sec\n')
+
+    pos_result = result[0].strip('\n\n')  # get first line, remove final double carriage return
+    pos_result = pos_result.split('\n\n')  # split messages by double carriage returns
+    pos_results = [pr.split('\n') for pr in pos_result]  # split parts of message by each carriage return
+    return pos_results
+
+
+def runtagger_parse(tweets, run_tagger_cmd=RUN_TAGGER_CMD):
+    """Call runTagger.sh on a list of tweets, parse the result, return lists of tuples of (term, type, confidence)"""
+    pos_raw_results = _call_runtagger(tweets, run_tagger_cmd)
+    pos_result = []
+    for pos_raw_result in pos_raw_results:
+        pos_result.append([x for x in _split_results(pos_raw_result)])
+    return pos_result
+
+
+def check_script_is_present(run_tagger_cmd=RUN_TAGGER_CMD):
+    """Simple test to make sure we can see the script"""
+    try:
+        po = subprocess.Popen([run_tagger_cmd, '--help'], stdout=subprocess.PIPE)
+        while not po.poll():
+            lines = [l for l in po.stdout]
+        # we expected the first line of --help to look like the following:
+        assert "RunTagger [options]" in lines[0]
+    except OSError as err:
+        print "Caught an OSError, have you specified the correct path to runTagger.sh? We are using \"%s\". Exception: %r" % (run_tagger_cmd, repr(err))
+
+
+if __name__ == "__main__":
+    print "Checking that we can see \"%s\", this will crash if we can't" % (RUN_TAGGER_CMD)
+    check_script_is_present()
+
+    print "Success."
+    print "Now pass in two messages, get a list of tuples back:"
+    tweets = ['this is a message', 'and a second message']
+    print runtagger_parse(tweets)
--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 ark-tweet-nlp-python
 ====================
 
-Simple Python wrapper around runTagger.sh of ark-tweet-nlp
+Simple Python wrapper around runTagger.sh of ark-tweet-nlp. It passes a list of tweets to runTagger.sh and parses the result into a list of lists of tuples, each tuple represents the (token, type, conficence).
+
+Usage:
+   
+    >>> import CMUTweetTagger
+    >>> print CMUTweetTagger.runtagger_parse(['example tweet 1', 'example tweet 2'])
+    >>> [[('example', 'N', 0.979), ('tweet', 'V', 0.7763), ('1', '$', 0.9916)], [('example', 'N', 0.979), ('tweet', 'V', 0.7713), ('2', '$', 0.5832)]]
+
+Note, if you receive:
+
+    >>> ValueError: Cannot find "./runTagger.sh"
+
+Make sure you pass in the correct path to runTagger.sh, e.g. if this script is cloned into a subdirectory of ark-tweet-nlp then you may need to use:
+
+    >>> print CMUTweetTagger.runtagger_parse(['example tweet 1', 'example tweet 2'], run_tagger_cmd="../runTagger.sh")
